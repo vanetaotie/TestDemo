53d7e9084f2619c11a0c9a7f4d6b821c847011ae
zhongk <vanetaotie@gmail.com>
2015-10-28 18:04:20 +0800

    scrollviewtest

--- TestDemo/ScrollTextLabel/ScrollTextViewController.m
+++ TestDemo/ScrollTextLabel/ScrollTextViewController.m
@@ -8,42 +8,82 @@
 
 #import "ScrollTextViewController.h"
 
-@interface ScrollTextViewController ()
+@interface ScrollTextViewController () <UIScrollViewDelegate>
 
 @end
 
 @implementation ScrollTextViewController
+{
+    CGFloat mainOffSet;
+    UILabel *leftSign;
+    UILabel *rightSign;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     [self.view setBackgroundColor:[UIColor whiteColor]];
     
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
     CGFloat height = 45;
     
     UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(10, 200, CGRectGetWidth(self.view.bounds)-20, height)];
-    scrollView.backgroundColor = [UIColor yellowColor];
+    scrollView.delegate = self;
+//    scrollView.backgroundColor = [UIColor yellowColor];
     scrollView.showsHorizontalScrollIndicator = NO;
     scrollView.showsVerticalScrollIndicator = NO;
     scrollView.bounces = NO;
     [self.view addSubview:scrollView];
     
     UILabel *contentLabel = [[UILabel alloc] initWithFrame:CGRectZero];
-    contentLabel.backgroundColor = [UIColor redColor];
-    contentLabel.textColor = [UIColor blackColor];
+//    contentLabel.backgroundColor = [UIColor redColor];
     contentLabel.font = [UIFont systemFontOfSize:14.0];
     
     //set
     NSString *textString = @"国家电网/江苏省电网公司/江苏省电力信息技术有限公司/开发部/综合组";
     contentLabel.text = textString;
     CGSize size = [textString sizeWithAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:14.0]}];
-//    if (size.width <= scrollView.frame.size.width) {
-        contentLabel.frame = CGRectMake(0, 0, size.width, height);
-//    } else {
-//        contentLabel.frame = CGRectMake(CGRectGetWidth(scrollView.frame)-size.width, 0, size.width, height);
-//    }
+    
+    contentLabel.frame = CGRectMake(0, 0, size.width, height);
     scrollView.contentSize = CGSizeMake(size.width, height);
+    if (size.width > scrollView.frame.size.width) {
+        mainOffSet = size.width-CGRectGetWidth(scrollView.frame);
+        [scrollView setContentOffset:CGPointMake(mainOffSet, 0)];
+        //sign
+        leftSign = [[UILabel alloc] initWithFrame:CGRectMake(CGRectGetMinX(scrollView.frame), CGRectGetMinY(scrollView.frame), 15, height)];
+        leftSign.text = @"...";
+        leftSign.backgroundColor = [UIColor whiteColor];
+        leftSign.hidden = NO;
+        [self.view addSubview:leftSign];
+        
+        rightSign = [[UILabel alloc] initWithFrame:CGRectMake(CGRectGetMaxX(scrollView.frame)-15, CGRectGetMinY(scrollView.frame), 15, height)];
+        rightSign.text = @"...";
+        rightSign.backgroundColor = [UIColor whiteColor];
+        rightSign.hidden = YES;
+        [self.view addSubview:rightSign];
+    }
+    
     [scrollView addSubview:contentLabel];
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    CGFloat offSet = scrollView.contentOffset.x;
+    NSLog(@"offSet:%f",offSet);
+    if (offSet >= mainOffSet) {
+        NSLog(@"已经滑到最右边");
+        leftSign.hidden = NO;
+        rightSign.hidden = YES;
+        return;
+    } else if (offSet == 0) {
+        NSLog(@"已经滑到最左边");
+        leftSign.hidden = YES;
+        rightSign.hidden = NO;
+        return;
+    }
+    leftSign.hidden = NO;
+    rightSign.hidden = NO;
 }
 
 - (void)didReceiveMemoryWarning {
